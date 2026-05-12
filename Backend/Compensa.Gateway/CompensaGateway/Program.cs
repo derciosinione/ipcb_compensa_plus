25b734fb448612d266de6f957de8668b901c4a69
derciosinione <derciosinione@gmail.com>
2026-05-12 05:29:47 +0100

    feat(gateway): add rate limiting to API Gateway

--- Backend/Compensa.Gateway/CompensaGateway/Program.cs
+++ Backend/Compensa.Gateway/CompensaGateway/Program.cs
@@ -1,8 +1,25 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add YARP
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Add Rate Limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("DefaultPolicy", opt =>
+    {
+        opt.Window = TimeSpan.FromSeconds(60);
+        opt.PermitLimit = 100; // 100 requests per minute
+        opt.QueueLimit = 20;
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+});
 
 // Add CORS
 builder.Services.AddCors(options =>
@@ -19,6 +36,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseCors("GatewayPolicy");
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
